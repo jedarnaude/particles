e26007e883a5773ad1628ac6a89a489b0b4b1e2d
Jose Enrique D'Arnaude <jedarnaude@prezi.com>
2014-09-06 16:08:06 +0200

    better xcode project with prebuild libraries for testing inside

---
+++ test/ParticleDataTest.cpp
@@ -0,0 +1,9 @@
+//
+//  ParticleDataTest.cpp
+//  particles
+//
+//  Created by Jose Enrique D'Arnaude on 9/6/14.
+//  Copyright (c) 2014 jedarnaude. All rights reserved.
+//
+
+#include "ParticleDataTest.h"
